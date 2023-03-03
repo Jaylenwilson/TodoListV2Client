11e7a0b998c85680af0ee7dfa8be8a74a0b7b8c9
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-03-02 22:01:12 -0500

    added some more styling next I will work on validation and check that the endpoints I am fetching work correctly

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -68,28 +68,33 @@ export default function Auth(props) {
 
   return (
     <>
-      <div className=" w-full bg-cover bg-center relative bg-gradient-to-tr from-purple-600 h-96">
-        <img className="w-full h-full object-cover absolute mix-blend-overlay" src="https://media.istockphoto.com/id/1185878239/photo/i-can-check-off-everything-on-my-to-do-list.jpg?b=1&s=170667a&w=0&k=20&c=Ggx0gruN6lSeQ22awYd5NBlevaz8KEDG24BZPQUqwr4=" alt="" />
+      <div className=" w-full bg-cover bg-center relative bg-gradient-to-tr from-blue-700 h-96">
+        <img className="blur-sm w-full h-full object-cover absolute mix-blend-overlay" src="https://media.istockphoto.com/id/496207390/photo/writing-to-do-list-of-a-day-is-a-good-habit.jpg?b=1&s=170667a&w=0&k=20&c=Mk3WZTtiYvBP6GedoL1OI1C6uTppgOS7chYbhWBEB1w=" alt="" />
+        <div className="p-24">
+          <h1 className="text-white text-5xl font-bold"> Take Control of Your Schedule </h1>
+          <h2 className="text-white text-3xl font-light">Simplify Your Day with Our To-Do List App</h2>
+        </div>
       </div>
-      <div className=' flex flex-col  items-center  ' >
+      <div className=' h-max flex flex-col items-center my-3' >
         {
 
           loginActive ?
 
-            <form className=" border rounded-md border-slate-200 p-5" onSubmit={basicLogin()}>
+            <form onSubmit={basicLogin()}>
+              <h1 className=" tracking-wider text-blue-500 text-4xl font-bold mb-6">Login</h1>
               <div className="grid gap-6 mb-6 md:grid-cols-2">
                 <div>
-                  <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                  <input type="text" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
+                  <label for="username" className="block mb-2 text-sm font-medium text-blue-900 dark:text-white">Username</label>
+                  <input type="text" id="username" className="bg-blue-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
                 </div>
                 <div>
-                  <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                  <input type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Example@email.com" required />
+                  <label for="email" className="block mb-2 text-sm font-medium text-blue-900 dark:text-white">Email</label>
+                  <input type="text" id="email" className="bg-blue-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Example@email.com" required />
                 </div>
               </div>
               <div class="mb-6">
-                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+                <label for="password" className="block mb-2 text-sm font-medium text-blue-900 dark:text-white">Password</label>
+                <input type="password" id="password" className="bg-blue-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
               </div>
               <div class="flex items-start mb-6">
                 <a onClick={toggleForm} className="text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
@@ -97,19 +102,21 @@ export default function Auth(props) {
               <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
 
-            : <form onSubmit={basicSignUp()}>
+            : <form className="w-6/12" onSubmit={basicSignUp()}>
+              <h1 className="text-4xl font-bold mb-6 text-blue-500">Get Started !</h1>
+
               <div class="grid gap-6 mb-6 md:grid-cols-2">
                 <div>
-                  <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                  <input type="text" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
+                  <label for="username" className="block mb-2 text-sm font-medium text-blue-900 dark:text-white">Username</label>
+                  <input type="text" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
                 </div>
                 <div>
-                  <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                  <label for="email" className="block mb-2 text-sm font-medium text-blue-900 dark:text-white">Email</label>
                   <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Example@email.com" required />
                 </div>
               </div>
               <div class="mb-6">
-                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <label for="password" className="block mb-2 text-sm font-medium text-blue-900 dark:text-white">Password</label>
                 <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
               </div>
 
