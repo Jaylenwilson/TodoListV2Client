105ed0863249729c41de652347ccfcef4ed06314
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-03-08 19:18:29 -0500

    created navbar with logo and profile pic which will be functional in version two

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react'
 import { Navigate } from 'react-router';
+import Logo from '../assets/donzo-b-w-logo.png'
 
 export default function Sidebar(props) {
     const [dropdownActive, setDropDownActive] = useState(false)
@@ -10,11 +11,69 @@ export default function Sidebar(props) {
         console.log(dropdownActive)
     }
 
+
     return (
 
         <div>
-
             {props.sessionToken ?
+                <nav>
+                    <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center justify-start">
+                                    {/* <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                                        <span className="sr-only">Open sidebar</span>
+                                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                                        </svg>
+                                    </button> */}
+                                    <div className="flex ml-2 md:mr-24">
+                                        <img src={Logo} class="mr-3 h-10" alt="Task solver logo" />
+                                        {/* <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span> */}
+                                    </div>
+                                </div>
+                                <div className="flex items-center">
+                                    <div className="flex items-center ml-3">
+                                        <div>
+                                            <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                                                <span className="sr-only">Open user menu</span>
+                                                <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo" />
+                                            </button>
+                                        </div>
+                                        <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
+                                            <div className="px-4 py-3" role="none">
+                                                <p className="text-sm text-gray-900 dark:text-white" role="none">
+                                                    Neil Sims
+                                            </p>
+                                                <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                                    neil.sims@flowbite.com
+                                            </p>
+                                            </div>
+                                            <ul className="py-1" role="none">
+                                                <li>
+                                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </nav>
+                : null}
+            {props.sessionToken ?
+
+
 
                 <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
                     <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
@@ -66,29 +125,6 @@ export default function Sidebar(props) {
                                 </button>
                                 {props.sessionToken === "" && <Navigate to='/' />}
                             </li>
-                            {/* <li>
-                                <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
-                                    <span className="flex-1 ml-3 whitespace-nowrap">Projects</span>
-                                    <span class="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">+</span>
-                                </a>
-                            </li> */}
-
-                            {/* <li>
-                            <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-                            </a>
-                        </li> */}
                         </ul>
                     </div>
                 </aside>
